56f70eedebb55718137c07819c2340e396935d53
Gustavo Freitas <gustavolfre@gmail.com>
2021-01-30 21:21:32 -0300

    adicionando informações na atividade

--- testeOrcamento.java
+++ testeOrcamento.java
@@ -12,15 +12,32 @@ public class testeOrcamento {
 		
 		LocalDateTime agora1 = LocalDateTime.now();
 		
+		String mensagemTotal = "Seu saldo total";
+		
 		Moeda salario1= new Moeda ("5000.00");
 		String salarioComentario = "Recebimento do Salário do mês de janeiro";
+		Orcamento salario = new Orcamento(debito1, salario1.getValorFormatado(), agora1, salarioComentario);
+		System.out.println(salario);
 		
 		Moeda aluguel1 = new Moeda ("1000.00");
 		salario1.substrairCom("1000.00");
-	
+		String pagamentoAluguel = "Pagamento do Aluguel";
+		Orcamento saldoAluguel = new Orcamento(credito1, aluguel1.getValorFormatado(), agora1, pagamentoAluguel);
+		System.out.println(saldoAluguel);
 		
-		Orcamento salario = new Orcamento(debito1, salario1.getValorFormatado(), agora1, salarioComentario);
-		System.out.println(salario);
+		
+		Orcamento total = new Orcamento (total1, salario1.getValorFormatado(), agora1, mensagemTotal );
+		System.out.println(total);
+		
+		System.out.println();
+		
+		Moeda contaLuz = new Moeda ("300.00");
+		salario1.substrairCom("300");
+		String pagamentoContaLuz = "Pagamento conta de Luz";
+		Orcamento saldoContaLuz = new Orcamento (credito1, contaLuz.getValorFormatado(), agora1, pagamentoContaLuz);
+		System.out.println(saldoContaLuz);
+		Orcamento total2 = new Orcamento (total1, salario1.getValorFormatado(), agora1, mensagemTotal );
+		System.out.println(total2);
 		
 		
 	}
